13bf14176a572e24a5f0cd4b0d0667b8eba21df1
patrick-hutchinson <hutchinsonpatrick@icloud.com>
2024-09-16 15:28:38 +0200

    more flexibility adjustments

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -12,6 +12,30 @@ export default function Contact({ data }) {
     }
   }, [data]);
 
+  function handleMouseEnter(e) {
+    const cursorImage = document.querySelector(".cursorImage > img");
+    cursorImage.classList.remove("pulseCursor");
+    cursorImage.classList.add("pulseCursor");
+
+    const handleAnimationEnd = () => {
+      cursorImage.classList.remove("pulseCursor");
+      cursorImage.removeEventListener("animationend", handleAnimationEnd);
+    };
+
+    cursorImage.addEventListener("animationend", handleAnimationEnd);
+  }
+
+  function handleSeeMoreMouseLeave(e) {
+    const cursorImage = document.querySelector(".cursorImage > img");
+
+    const handleAnimationEnd = () => {
+      cursorImage.classList.remove("pulseCursor");
+      cursorImage.removeEventListener("animationend", handleAnimationEnd);
+    };
+
+    cursorImage.addEventListener("animationend", handleAnimationEnd);
+  }
+
   return (
     <AnimatedPage>
       <main className="pageContainer contactPage">
@@ -22,16 +46,27 @@ export default function Contact({ data }) {
             </video>
           )}
         </div>
+
         <div className="contact">
-          <div className="contacts-specific"></div>
+          {/* <div className="desktop"></div> */}
           <div className="socials">
             <div>
-              <a href="mailto:ciao@gg-office.com" target="blank">
+              <a
+                href="mailto:ciao@gg-office.com"
+                target="blank"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleSeeMoreMouseLeave}
+              >
                 SHOOT US AN EMAIL
               </a>
             </div>
             <div className="instagram">
-              <a href="https://www.instagram.com/gg__rugs/" target="blank">
+              <a
+                href="https://www.instagram.com/gg__rugs/"
+                target="blank"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleSeeMoreMouseLeave}
+              >
                 IG
               </a>
             </div>
